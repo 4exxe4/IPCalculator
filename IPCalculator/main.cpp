ea15441b8744012dcb68b6e3b4214a1b2c321b80
user <user@WIN-CP209TC39O5>
2026-05-29 02:20:47 +0300

    IPcalc almost DONE

--- IPCalculator/main.cpp
+++ IPCalculator/main.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include<CommCtrl.h>
 #include<iostream>
@@ -11,6 +11,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, (DLGPROC)DlgProc, NULL);
 	return 0;
 }
+
+VOID PrintInfo(HWND hwnd);
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -46,9 +48,6 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			{
 				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
 				//std::cout << FIRST_IPADDRESS(dwIPaddress) << std::endl;
-				if (FIRST_IPADDRESS(dwIPaddress) < 128)dwIPmask = 0xFF000000;// , dwIPprefix = 8;
-				else if (FIRST_IPADDRESS(dwIPaddress) < 192)dwIPmask = 0xFFFF0000;// , dwIPprefix = 16;
-				else if (FIRST_IPADDRESS(dwIPaddress) < 224)dwIPmask = 0xFFFFFF00;// , dwIPprefix = 24;
 				if (FIRST_IPADDRESS(dwIPaddress) < 128)dwIPmask = 0xFF000000, dwIPprefix = 8;
 				else if (FIRST_IPADDRESS(dwIPaddress) < 192)dwIPmask = 0xFFFF0000, dwIPprefix = 16;
 				else if (FIRST_IPADDRESS(dwIPaddress) < 224)dwIPmask = 0xFFFFFF00, dwIPprefix = 24;
@@ -77,7 +76,6 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			}
 		}
 		break;
-		/*case IDC_EDIT_PREFIX:
 		case IDC_EDIT_PREFIX:
 		{
 			DWORD dwIPaddress = 0;
@@ -94,9 +92,14 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
 			}
 		}
-			break;*/
 		break;
+		case IDC_BUTTON_RESET:
+			SendMessage(hIPaddress, IPM_CLEARADDRESS, 0, 0);
+			SendMessage(hIPmask, IPM_CLEARADDRESS, 0, 0);
+			SendMessage(hIPprefix, WM_SETTEXT, 0, (LPARAM)"");
+			break;
 		case IDOK:
+			PrintInfo(hwnd);
 			break;
 		case IDCANCEL:EndDialog(hwnd, 0);
 		}
@@ -105,8 +108,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_NOTIFY:
 	{
 		NMHDR* p_nmhdr = (NMHDR*)lParam;
-		if (p_nmhdr->idFrom == IDC_IP_MASK)
-			std::cout << p_nmhdr->idFrom << "\t" << wParam << std::endl;
+		std::cout << p_nmhdr->idFrom << "\t" << wParam << std::endl;
 		if (wParam == IDC_IP_MASK || wParam == IDC_IP_ADDRESS)
 			//if (p_nmhdr->hwndFrom == GetDlgItem(hwnd, IDC_IP_MASK))
 		{
@@ -124,3 +126,51 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CLOSE:	EndDialog(hwnd, 0);
 	}
 	return FALSE;
+}
+LPSTR FormatAddress(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwIPaddress)
+{
+	sprintf
+	(
+		szBuffer,
+		"%s%i.%i.%i.%i",
+		szMessage,
+		FIRST_IPADDRESS(dwIPaddress),
+		SECOND_IPADDRESS(dwIPaddress),
+		THIRD_IPADDRESS(dwIPaddress),
+		FOURTH_IPADDRESS(dwIPaddress)
+	);
+	return szBuffer;
+}
+LPSTR FormatCount(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwCount)
+{
+	sprintf(szBuffer, "%s%i", szMessage, dwCount);
+	return szBuffer;
+}
+VOID PrintInfo(HWND hwnd)
+{
+	HWND hIPaddress = GetDlgItem(hwnd, IDC_IP_ADDRESS);
+	HWND hIPmask = GetDlgItem(hwnd, IDC_IP_MASK);
+	HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
+	DWORD dwIPaddress = 0;
+	DWORD dwIPmask = 0;
+	SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+	SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+	DWORD dwNetworkAddress = dwIPaddress & dwIPmask;
+	DWORD dwBroadcastAddress = dwIPaddress | ~dwIPmask;
+
+	CHAR szInfo[1024] = {};
+	CHAR szNetworkAddress[1024] = {};
+	CHAR szBroadcastAddress[1024] = {};
+	CHAR szIPcount[1024] = {};
+	CHAR szHostCount[1024] = {};
+	sprintf
+	(
+		szInfo,
+		"%s;\n%s;\n%s;\n%s;\n",
+		FormatAddress(szNetworkAddress, "Адрес сети:\t\t\t", dwNetworkAddress),
+		FormatAddress(szBroadcastAddress, "Широковещательный адрес:\t", dwBroadcastAddress),
+		FormatCount(szIPcount, "Количество IP-адресов:\t", dwBroadcastAddress - dwNetworkAddress + 1),
+		FormatCount(szHostCount, "Количество узлов:\t\t", dwBroadcastAddress - dwNetworkAddress - 1)
+	);
+	SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)szInfo);
+}
